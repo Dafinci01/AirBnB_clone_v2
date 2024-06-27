9b9cbe560cd2e4ce7d8f42f298c6a3b0eea4ece1
Dafinci01 <odelanadavid@gmail.com>
2024-06-27 08:16:16 +0100

    city

---
+++ amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Amenity"""
+from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class Amenity(BaseModel, Base):
+    """This class defines an amenity by various attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    """ place_amenities = relationship("Place", secondary='place_amenity',
+                                   viewonly=False)
+    """                                                               
