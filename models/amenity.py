29da5af251ab483a91707999bf78e6b44a382c62
Dafinci01 <odelanadavid@gmail.com>
2024-06-27 05:47:38 +0100

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines a class Amenity"""
+from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This class defines an amenity by various attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    """ place_amenities = relationship("Place", secondary='place_amenity',
+                                   viewonly=False)
+    """
