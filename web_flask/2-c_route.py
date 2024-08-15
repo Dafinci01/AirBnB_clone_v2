054743e4f98025e3cf0f290553158160b1fb46f2
Dafinci01 <odelanadavid@gmail.com>
2024-08-15 18:05:16 +0100

    web app that display c_is_fun text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display, 'HBNB'"""
+    return "HBNB"
+
+@app.route("/c/<test>", strict_slashes=False)
+def c(text):
+    """Display 'C' followed by text <text>"""
+    return "C {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")     
